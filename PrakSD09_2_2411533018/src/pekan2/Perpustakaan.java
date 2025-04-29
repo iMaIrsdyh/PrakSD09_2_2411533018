22cee9586b98d90716b955b0a371514a11f87e5b
iMaIrsdyh <karimahirsyadiyah@gmail.com>
2025-04-29 12:10:58 +0700

    Pekan 2 Pratikum Struktur Data

--- PrakSD09_2_2411533018/src/pekan2/Perpustakaan.java
+++ PrakSD09_2_2411533018/src/pekan2/Perpustakaan.java
@@ -1,31 +1,70 @@
 package pekan2;
+
 import java.util.*;
 
-public class Perpustakaan {
-	class Buku {
-		String judul, pengarang, isbn;
-		Buku (String judul, String pengarang, String isbn) {
-			this.judul = judul;
-			this.pengarang = pengarang;
-			this.isbn = isbn;
-		}
+class Buku{
+	String judul, pengarang, isbn;
+	Buku (String judul, String pengarang, String isbn) {
+		this.judul = judul;
+		this.pengarang = pengarang;
+		this.isbn = isbn;
 	}
-		LinkedList<Buku> koleksiBuku = new LinkedList<>();
-		Queue<Buku> Peminjaman = new LinkedList<>();
-		Stack<Buku> Pengembalian = new Stack<>();
-		
-//menggunakan LinkedList		
-		void tambahBuku(String judul, String pengarang, String isbn) {
-			koleksiBuku.add(new Buku(judul, pengarang, isbn));
-	}
+}
 
-//Menggunakan queue
-		void pinjamBuku(String judul) {
-			for (Buku buku :koleksiBuku) {
-				if (buku.judul.equals(judul)) {
-					Peminjaman.add(buku);
-					break;
-				}
+class Perpustakaan {
+	LinkedList<Buku> koleksiBuku = new LinkedList<>();
+	Queue<Buku> Peminjaman = new LinkedList<>();
+	Stack<Buku> Pengembalian = new Stack<>();
+// use the linkedlist
+	void tambahBuku(String judul, String pengarang, String isbn) {
+		koleksiBuku.add (new Buku(judul, pengarang, isbn));
+	}
+// use the queue
+	void pinjamBuku (String judul) {
+		for (Buku buku : koleksiBuku) {
+			if (buku.judul.equals(judul)) {
+				Peminjaman.add(buku);
+				break;
 			}
 		}
+	}
+// use the stack		
+	void kembalikanBuku (String judul) {
+		for (Buku buku : Peminjaman) {
+			if (buku.judul.equals(judul)) {
+				Pengembalian.push(buku);
+				break;
+			}
+		}
+	}
+	public static void main (String[] args) {
+		Perpustakaan perpustakaan = new Perpustakaan();
+		Scanner scanner = new Scanner (System.in);
+		while (true) {
+			System.out.println(" 1. Tambah Buku\n 2. Pinjam Buku\n " + "3. Kembalikan Buku\n 4. keluar");
+			System.out.print("Pilih opsi: ");
+			int pilihan = scanner.nextInt();
+			scanner.nextLine();
+			if (pilihan == 1) {
+				System.out.print("Masukkan judul: ");
+				String judul = scanner.nextLine();
+				System.out.print("Masukkan pengarang: ");
+				String pengarang = scanner.nextLine();
+				System.out.print("Masukkan ISBN: ");
+				String isbn = scanner.nextLine();
+				perpustakaan.tambahBuku(judul, pengarang, isbn);
+			}else if (pilihan == 2) {
+				System.out.print("Masukkan judul buku yang ingin dipinjam: ");
+				String judul = scanner.nextLine();
+				perpustakaan.pinjamBuku(judul);
+			}else if (pilihan == 3) {
+				System.out.print("Masukkan judul buku yang ingin dikembalikan: ");
+				String judul = scanner.nextLine();
+				perpustakaan.kembalikanBuku(judul);
+			}else if (pilihan == 4)	{
+				break;
+			}
+		}
+		scanner.close();
+	}
 }
