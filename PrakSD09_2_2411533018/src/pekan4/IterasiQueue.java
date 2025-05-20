6d2c8cb3334a7bd0897df139cddb94da2ff4ea1c
iMaIrsdyh <karimahirsyadiyah@gmail.com>
2025-05-20 12:10:47 +0700

    Pratikum Struktur Data Pekan 4

--- PrakSD09_2_2411533018/src/pekan4/IterasiQueue.java
+++ PrakSD09_2_2411533018/src/pekan4/IterasiQueue.java
@@ -1,5 +1,23 @@
 package pekan4;
 
-public class IterasiQueue {
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class IterasiQueue {
+    public static void main(String args[]) {
+        Queue<String> q = new LinkedList<>();
+
+        q.add("Praktikum");
+        q.add("Struktur");
+        q.add("Data");
+        q.add("Dan");
+        q.add("Algoritma");
+
+        Iterator<String> iterator = q.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+    }
 }
+
