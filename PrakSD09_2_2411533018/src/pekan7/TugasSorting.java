07b1138ff6b7677cffaff867cc6b854c4714c4a5
iMaIrsdyh <karimahirsyadiyah@gmail.com>
2025-06-10 00:04:08 +0700

    Tugas Sorting Pekan 7

--- PrakSD09_2_2411533018/src/pekan7/TugasSorting.java
+++ PrakSD09_2_2411533018/src/pekan7/TugasSorting.java
@@ -3,31 +3,29 @@ package pekan7;
 public class TugasSorting {
     public static void main(String[] args) {
         char[] a = new char[26];
-        int b = 17;
-
+        int b = 18; // Jumlah elemen yang akan diurutkan
         // Inisialisasi array dengan huruf dari z ke a
         for (int i = 0; i < 26; i++) {
             a[i] = (char) ('z' - i);
         }
-        // Panggil metode insertion sort untuk n elemen pertama
-        is(a, b);
-
-        // Cetak seluruh isi array setelah sorting
+        // Panggil metode bubble sort untuk 18 elemen pertama
+        bubbleSort(a, b);
+        // Cetak seluruh isi array setelah sorting sebagian
         for (int i = 0; i < 26; i++) {
             System.out.print(a[i] + " ");
         }
     }
-    // Metode insertion sort
-    public static void is(char[] a, int n) {
-        for (int i = 1; i < n; i++) {
-            char key = a[i];
-            int j = i - 1;
-
-            while (j >= 0 && a[j] > key) {
-                a[j + 1] = a[j];
-                j--;
+    // Metode bubble sort untuk n elemen pertama dari array
+    public static void bubbleSort(char[] a, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (a[j] > a[j + 1]) {
+                    // Tukar elemen
+                    char temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
             }
-            a[j + 1] = key;
         }
     }
 }
